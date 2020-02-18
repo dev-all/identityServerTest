0386582e557f9c559f2c18079ea1f0398afaaac6
Leonardo <developer@gmail.com>
2020-02-17 23:42:08 -0300

    first commit

---
+++ All.Identity.Server/Config.cs
@@ -0,0 +1,57 @@
+﻿using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace All.Identity.Server
+{
+    public static class Config
+    {
+
+
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new ApiResource[]
+            {
+                new ApiResource("reourceAll", "Recurso al q puede acceder el cliente ")
+
+
+            };
+        }
+
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new[]
+            {
+                // client credentials flow client
+                new Client
+                {
+                    ClientId = "client1",
+                    ClientName = "Sebastian",
+
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets = { new Secret("claveQueSeDaAlCliente".Sha256()) },
+
+                    AllowedScopes = { "reourceAll" }
+                },
+                
+                // client credentials flow client
+                new Client
+                {
+                    ClientId = "client2",
+                    ClientName = "Mauricio",
+
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets = { new Secret("claveParaMauricio".Sha256()) },
+
+                    AllowedScopes = { "reourceAll" }
+                }
+
+            };
+        }
+
+
+    }
+}
